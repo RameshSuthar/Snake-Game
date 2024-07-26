777e43dc90ccc4edd2b0cb274abd22fdee4b5707
Ramesh Suthar <Rameshsuthar1231@gmail.com>
2024-07-26 14:29:41 +0530

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Input = ({ type, placeholder, value, onChange, label }) => {
+const Input = ({ type, placeholder, value, onChange, label, min }) => {
     return (
         <div className="field">
             <label>{label}</label>
-            <input type={type} placeholder={placeholder} value={value} onChange={onChange} />
+            <input min={min} type={type} placeholder={placeholder} value={value} onChange={onChange} />
         </div>
     );
 }
