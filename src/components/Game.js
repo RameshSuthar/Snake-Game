76747c8a24ff95540452fb6ba6406894bfc158fc
Ramesh Suthar <Rameshsuthar1231@gmail.com>
2024-07-26 14:29:23 +0530

    Update Game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,7 +6,6 @@ import Input from "./Input";
 const Game = () => {
     const [noOfRows, setNoOfRows] = useState(20);
     const [noOfColumns, setNoOfColumns] = useState(30);
-    const [minimalBoxSize, setMinimalBoxSize] = useState(20);
     const [speedIncrement, setSpeedIncrement] = useState(20);
     const [initialSnakeSpeed, setInitialSnakeSpeed] = useState(200);
     const [noOfGrabs, setNoOfGrabs] = useState(2);
@@ -17,22 +16,20 @@ const Game = () => {
             <ScoreCard />
             <div className="customization-wrapper">
                 <div className="row">
-                    <Input type="number" value={noOfColumns} onChange={(e) => setNoOfColumns(parseInt(e.target.value))} label="Columns: "/>
-                    <Input type="number" value={noOfRows} onChange={(e) => setNoOfRows(parseInt(e.target.value))} label="Rows: "/>
+                    <Input type="number" min={1} value={noOfColumns} onChange={(e) => setNoOfColumns(parseInt(e.target.value))} label="Columns: "/>
+                    <Input type="number" min={1} value={noOfRows} onChange={(e) => setNoOfRows(parseInt(e.target.value))} label="Rows: "/>
                 </div>
                 <div className="row">
-                    <Input type="number" value={minimalBoxSize} onChange={(e) => setMinimalBoxSize(parseInt(e.target.value))} label="Single Box Size in Board: "/>
-                    <Input type="number" value={speedIncrement} onChange={(e) => setSpeedIncrement(parseInt(e.target.value))} label="Speed Increment in ms: "/>
+                    <Input type="number" min={1} value={initialSnakeSpeed} onChange={(e) => setInitialSnakeSpeed(parseInt(e.target.value))} label="Initial Snake Speed in ms: "/>
+                    <Input type="number" min={1} value={noOfGrabs} onChange={(e) => setNoOfGrabs(parseInt(e.target.value))} label="Number of Grabs: "/>
                 </div>
                 <div className="row">
-                    <Input type="number" value={initialSnakeSpeed} onChange={(e) => setInitialSnakeSpeed(parseInt(e.target.value))} label="Initial Snake Speed in ms: "/>
-                    <Input type="number" value={noOfGrabs} onChange={(e) => setNoOfGrabs(parseInt(e.target.value))} label="Number of Grabs: "/>
+                    <Input type="number" min={1} value={speedIncrement} onChange={(e) => setSpeedIncrement(parseInt(e.target.value))} label="Speed Increment in ms: "/>
                 </div>
             </div>
             <GameBoard 
                 rows={noOfRows} 
                 columns={noOfColumns} 
-                minimalBoxSize={minimalBoxSize} 
                 speedIncrement={speedIncrement}
                 initialSnakeSpeed={initialSnakeSpeed}
                 numOfGrabs={noOfGrabs}
